3242a18378dbb8096c53fc991df40619c4237c4c
Collinb190 <collinb190@gmail.com>
2024-02-02 09:40:46 -0600

    I see now

--- models/base_model.py
+++ models/base_model.py
@@ -11,20 +11,11 @@ class BaseModel:
     Class that defines all common members for other classes.
     """
 
-    def __init__(self, *args, **kwargs) -> None:
+    def __init__(self) -> None:
         """Constructor method to create an instance of BaseModel."""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(
-                        value, "%Y-%m-%dT%H:%M:%S.%f"
-                    ))
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
 
     def __str__(self) -> str:
         """Gives a user friendly string representation of BaseModel."""
