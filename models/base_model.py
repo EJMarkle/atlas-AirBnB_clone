946e4cf442be52a1bb1e5644c5c8e80c519938de
Collinb190 <collinb190@gmail.com>
2024-02-02 08:27:19 -0600

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -18,6 +18,7 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self) -> str:
+        """Gives a user friendly string representation of BaseModel."""
         return "[{}] ({}) {}".format(
             self.__class__.__name__, self.id, self.__dict__
         )
@@ -25,3 +26,14 @@ class BaseModel:
     def save(self) -> None:
         """Updates the field updated_at with current datetime."""
         self.updated_at = datetime.datetime.now()
+
+    def to_dict(self) -> dict:
+        """Returns a dictionary rep of the instance."""
+        r_dict = self.__dict__.copy()
+        # Get only the instance fields.
+        r_dict['__class__'] = self.__class__.__name__
+        # Add the __class__ key with the class name of the object.
+        r_dict['created_at'] = self.created_at.isoformat()
+        r_dict['updated_at'] = self.updated_at.isoformat()
+        # Convert instance fields to ISO format string.
+        return r_dict
