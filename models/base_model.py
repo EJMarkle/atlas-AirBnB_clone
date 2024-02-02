0e6a161e32895bc1fc0a46d44b90f2573d6c01c8
Collinb190 <collinb190@gmail.com>
2024-02-02 08:10:49 -0600

    adjustment

--- models/base_model.py
+++ models/base_model.py
@@ -16,8 +16,12 @@ class BaseModel:
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = self.created_at
-        # Add this to methods that modify:
-        # self.updated_at = datetime.datetime.now()
 
     def __str__(self) -> str:
-        pass
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
+
+    def save(self) -> None:
+        """Updates the field updated_at with current datetime."""
+        self.updated_at = datetime.datetime.now()
