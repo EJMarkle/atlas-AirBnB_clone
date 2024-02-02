afecbe222ef3ecec5798c8a27d495f3f1a85b401
Collinb190 <collinb190@gmail.com>
2024-02-02 09:19:05 -0600

    fixing 3

--- models/base_model.py
+++ models/base_model.py
@@ -3,7 +3,7 @@
 This module will define the BaseModel class.
 """
 import uuid
-import datetime
+from datetime import datetime
 
 
 class BaseModel:
@@ -11,11 +11,20 @@ class BaseModel:
     Class that defines all common members for other classes.
     """
 
-    def __init__(self) -> None:
-        """Creates an instance of BaseModel."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs) -> None:
+        """Constructor method to create an instance of BaseModel."""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.strptime(
+                        value, "%Y-%m-%dT%H:%M:%S.%f"
+                    ))
+                else:
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self) -> str:
         """Gives a user friendly string representation of BaseModel."""
@@ -25,7 +34,7 @@ class BaseModel:
 
     def save(self) -> None:
         """Updates the field updated_at with current datetime."""
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self) -> dict:
         """Returns a dictionary rep of the instance."""
