1ca9ee492e6b22735dd8a189eb5fb3266fb96319
EJMarkle <emarkle99@gmail.com>
2024-02-03 17:01:50 -0600

    simple console

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This is the console that contains the entry point of the command interpreter.
+"""
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb)"
+    def do_quit(self, arg):
+        """
+        Exits the command interpreter
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        Exits the command interpreter with CTRL-D
+        """
+        print()
+        return True
+
+    def emptyline(self):
+        """
+        Empty line + ENTER does nothing
+        """
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
