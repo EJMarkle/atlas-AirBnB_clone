f49eac9947681b0e59e6763b30ce9bd376a7c6b7
Collinb190 <collinb190@gmail.com>
2024-02-03 01:06:48 -0600

    task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,10 +13,30 @@ class FileStorage:
 
     def all(self) -> dict:
         """Returns the dictionary __objects."""
-        return  self.__objects
-    
+        return self.__objects
+
     def new(self, obj) -> None:
         """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
-        
+
+    def save(self) -> None:
+        """Serializes __objects to the JSON file."""
+        seri_objects = {}
+        for key, obj in self.__objects.items():
+            seri_objects[key] = obj.to_dict()
+        with open(self.__file_path, 'w') as file:
+            json.dump(seri_objects, file)
+
+    def reload(self):
+        """"Deserializes the JSON file to __objects."""
+        try:
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, obj_dict in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj_class = globals()[class_name]
+                    obj = obj_class(**obj_dict)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
