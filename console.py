112af165b5c7ab1bf6860300bbdee8e89a7db7a5
EJMarkle <emarkle99@gmail.com>
2024-02-03 21:08:48 -0600

    excess imports on console

--- console.py
+++ console.py
@@ -5,12 +5,6 @@ This is the console that contains the entry point of the command interpreter.
 import cmd
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.user import User
 
 
 class HBNBCommand(cmd.Cmd):
