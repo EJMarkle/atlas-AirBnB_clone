ef63a1a268682c6dd8a31e450552fb7db00f786d
EJMarkle <emarkle99@gmail.com>
2024-02-03 19:00:17 -0600

    do update

--- console.py
+++ console.py
@@ -114,5 +114,46 @@ class HBNBCommand(cmd.Cmd):
             instances = [str(obj) for key, obj in storage.all().items() if key.startswith(class_name)]
             print(instances)
 
+    def do_update(self, arg):
+        """
+        Updates an instance based on the class name and id by adding or updating attribute
+        """
+        args = arg.split()
+        if not args:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in storage.all():
+            print("** class doesn't exist **")
+            return
+
+        if len(args) < 2:
+            print("** instance id missing **")
+            return
+
+        obj_id = args[1]
+        key = "{}.{}".format(class_name, obj_id)
+
+        if key not in storage.all():
+            print("** no instance found **")
+            return
+
+        if len(args) < 3:
+            print("** attribute name missing **")
+            return
+
+        attr_name = args[2]
+
+        if len(args) < 4:
+            print("** value missing **")
+            return
+
+        attr_value = args[3]
+
+        setattr(storage.all()[key], attr_name, type(getattr(storage.all()[key], attr_name))(attr_value))
+        storage.save()
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
